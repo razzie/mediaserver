7057dd53de534959727b93f6e18ae994cf7a76af
Gábor Görzsöny <gabor@gorzsony.com>
2020-04-27 18:46:57 +0200

    Keeping command-line arg values in global variables

--- main.go
+++ main.go
@@ -9,20 +9,28 @@ import (
 	"github.com/razzie/mediaserver/thumb"
 )
 
+// Command-line args
+var (
+	RedisAddr string
+	RedisPw   string
+	RedisDb   int
+	Port      int
+)
+
 func main() {
-	redisAddr := flag.String("redis-addr", "localhost:6379", "Redis hostname:port")
-	redisPw := flag.String("redis-pw", "", "Redis password")
-	redisDb := flag.Int("redis-db", 0, "Redis database (0-15)")
-	port := flag.Int("port", 8080, "http port to listen on")
+	flag.StringVar(&RedisAddr, "redis-addr", "localhost:6379", "Redis hostname:port")
+	flag.StringVar(&RedisPw, "redis-pw", "", "Redis password")
+	flag.IntVar(&RedisDb, "redis-db", 0, "Redis database (0-15)")
+	flag.IntVar(&Port, "port", 8080, "HTTP port to listen on")
 	flag.IntVar(&thumb.Quality, "-thumb-quality", 10, "Quality of the thumbnail images (1-100)")
 	flag.UintVar(&thumb.Size, "-thumb-size", 256, "Maximum width or height of thumbnail images")
 	flag.Parse()
 
-	db, err := NewDB(*redisAddr, *redisPw, *redisDb)
+	db, err := NewDB(RedisAddr, RedisPw, RedisDb)
 	if err != nil {
 		log.Fatalln("failed to connect to database:", err)
 	}
 
 	server := NewServer(db)
-	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(*port), server))
+	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(Port), server))
 }
